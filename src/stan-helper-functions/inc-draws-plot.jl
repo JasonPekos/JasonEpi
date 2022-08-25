23adc47801bf771a60ecbeb75ca543d1fd6f098b
Jason Pekos <78766845+JasonPekos@users.noreply.github.com>
2022-08-25 13:17:31 -0400

    quick fix

--- src/stan-helper-functions/inc-draws-plot.jl
+++ src/stan-helper-functions/inc-draws-plot.jl
@@ -24,23 +24,37 @@ function sampled_inc_plot(chain, data, num_plots)
     return p 
 end
 
-function sampled_plot(chain, data, num_plots, symbol)
+function sampled_plot(chain, data, num_plots, symbol, diff = false)
 
     p = plot()
-    
-    p = plot(Array(chain[namesingroup(chain, symbol)][1])[1,:],
-             color = :blue,
-             opacity = 0.1,
-             label = "I, accepted samples",
-             legend = :topleft );
+    if diff == false
+        p = plot(Array(chain[namesingroup(chain, symbol)][1])[1,:],
+                color = :blue,
+                opacity = 0.1,
+                label = "I, accepted samples",
+                legend = :topleft );
 
-    for i in 2:num_plots
-        plot!(Array(chain[namesingroup(chain, symbol)][i])[1,:],
-              color = :blue, opacity = 0.1,
-              label = "" )
+        for i in 2:num_plots
+            plot!(Array(chain[namesingroup(chain, symbol)][i])[1,:],
+                color = :blue, opacity = 0.1,
+                label = "" )
+        end
     end
-    plot!(data, color = 3, label = "")
-    plot!(data, seriestype = :scatter, color = :cyan, label = "data")
+    if diff == true
+        p = plot(diff(Array(chain[namesingroup(chain, symbol)][1])[1,:]),
+                color = :blue,
+                opacity = 0.1,
+                label = "I, accepted samples",
+                legend = :topleft );
+
+        for i in 2:num_plots
+            plot!(diff(Array(chain[namesingroup(chain, symbol)][i])[1,:]),
+                color = :blue, opacity = 0.1,
+                label = "" )
+        end
+    end
+    plot!(data[2:end], color = 3, label = "")
+    plot!(data[2:end], seriestype = :scatter, color = :cyan, label = "data")
     
 
     title!("Sampled Values")
